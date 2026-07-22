d0e3fc61ffbb6bba21edf59a7c89ee363c5717d2
Theneuralpeach <noreply@users.noreply.github.com>
2026-07-21 23:09:35 -0700

    Starry transfer booked for move-in day, mark task done

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'move-checklist-v16';
+const CACHE = 'move-checklist-v17';
 const ASSETS = ['./', './index.html', './manifest.json', './info.js', './settings.html', './floorplan.html', './bed-pickup.html', './starry-transfer.html'];
 
 self.addEventListener('install', e => {
